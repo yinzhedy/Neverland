0da4120976666054e5e0f04a664c3f61aa8a5615
yinzhedy <yinzhe.dy@gmail.com>
2023-01-18 20:46:20 -0500

    added useNewUrlParser in response to render logs error

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/neverland", 
-// { useNewUrlParser: true,
+{ useNewUrlParser: true
 // useUnifiedTopology: true,
 // useCreateIndex: true,
-// useFindAndModify: false }
+// useFindAndModify: false 
+}
 );
 
 module.exports = mongoose.connection
